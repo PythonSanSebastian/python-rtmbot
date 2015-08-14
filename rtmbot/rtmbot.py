cb03ae016144cd9e25756476593cc9f28d656956
Alexandre M. S <alexsavio@gmail.com>
2015-08-14 17:22:05 +0200

    Remove SlackBot declaration

--- rtmbot/rtmbot.py
+++ rtmbot/rtmbot.py
@@ -24,63 +24,6 @@ log = logging.getLogger(__name__)
 #JOB_HASH          = {}
 BOT               = []
 
-class SlackBot(object):
-
-    def __init__(self, token, ping_freq=3):
-        self.last_ping    = 0
-        self.token        = token
-        self.bot_plugins  = {}
-        self.slack_client = None
-        self._ping_freq   = ping_freq
-
-    def connect(self):
-        """Convenience method that creates Server instance"""
-        self.slack_client = SlackClient(self.token)
-        self.slack_client.rtm_connect()
-
-    def start(self):
-        self.connect()
-        #self.load_plugins()
-        while True:
-            for reply in self.slack_client.rtm_read():
-                self.input(reply)
-            self.crons()
-            self.output()
-            self.autoping()
-            time.sleep(.1)
-
-    def autoping(self):
-        now = int(time.time())
-        if now > self.last_ping + self._ping_freq:
-            self.slack_client.server.ping()
-            self.last_ping = now
-
-    def input(self, data):
-        if "type" in data:
-            # TODO: this logic in function calling is okay, but it does not allow me
-            # to call different functions in the same file (no overloading allowed).
-            # An approach with decorators would be much better.
-            function_name = "process_" + data["type"]
-            log.debug("got {}".format(function_name))
-            for plugin in self.bot_plugins:
-                plugin.register_jobs()
-                plugin.do(function_name, data)
-
-    def output(self):
-        for plugin in self.bot_plugins:
-            limiter = False
-            for output in plugin.do_output():
-                channel = self.slack_client.server.channels.find(output[0])
-                if channel != None and output[1] != None:
-                    if limiter == True:
-                        time.sleep(.1)
-                        limiter = False
-                    message = output[1].encode('ascii', 'ignore')
-                    channel.send_message("{}".format(message))
-                    limiter = True
-
-    
-
 class RtmBot(object):
 
     def __init__(self, token, ping_freq=3):
